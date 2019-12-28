9a9c4dd34df9b2d586e930415849357bfb51cbd6
Joanna Wytrzes <joannawytrzes@gmail.com>
2019-12-28 19:05:50 +0100

    Setup

---
+++ random-answer/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+        alt="asia"
+      >
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore ad veritatis velit vitae,
+        voluptatibus tenetur, exercitationem tempore quae commodi nam, quasi maiores ducimus
+        corporis obcaecati? Officiis blanditiis sed repellendus autem.
+      </a>
+    </header>
+  </div>
+);
+
+export default App;
