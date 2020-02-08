fefcdda569db60c1f415250bfe2df7d69bbc8ed4
Joanna Wytrzes <joannawytrzes@gmail.com>
2020-02-08 20:05:19 +0100

    Display single note

--- fav-note/src/views/DetailsPage.js
+++ fav-note/src/views/DetailsPage.js
@@ -1,24 +1,64 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import DetailsTemplate from 'templates/DetailsTemplate';
+import withContext from 'hoc/withContext';
+import { connect } from 'react-redux';
+import axios from 'axios';
+import PropTypes from 'prop-types';
 
-const dummyArticle = {
-  id: 1,
-  title: 'Wake me up when Vue ends',
-  content:
-    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
-  twitterName: 'hello_roman',
-  articleUrl: 'https://youtube.com/helloroman',
-  created: '1 day',
+const DetailsPage = ({ activeItem, match }) => {
+  const [item, setItem] = useState({
+    title: '',
+    content: '',
+    twitterName: '',
+    articleUrl: '',
+  });
+
+  useEffect(() => {
+    if (activeItem) {
+      const [newItem] = activeItem;
+      setItem(newItem);
+    } else {
+      const { id } = match.params;
+      axios
+        .get(`http://localhost:9000/api/note/${id}`)
+        .then(({ data }) => {
+          setItem(data);
+        })
+        .catch(err => console.log(err));
+    }
+  }, [activeItem]);
+
+  return (
+    <DetailsTemplate
+      title={item.title}
+      created={item.created}
+      content={item.content}
+      articleUrl={item.articleUrl}
+      twitterName={item.twitterName}
+    />
+  );
 };
 
-const DetailsPage = () => (
-  <DetailsTemplate
-    title={dummyArticle.title}
-    created={dummyArticle.created}
-    content={dummyArticle.content}
-    articleUrl={dummyArticle.articleUrl}
-    twitterName={dummyArticle.twitterName}
-  />
-);
+DetailsPage.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string.isRequired,
+    }),
+  }).isRequired,
+  activeItem: PropTypes.shape({
+    _id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    content: PropTypes.string.isRequired,
+    articleUrl: PropTypes.string.isRequired,
+  }).isRequired,
+};
 
-export default DetailsPage;
+const mapStateToProps = (state, ownProps) => {
+  if (state[ownProps.pageContext]) {
+    return {
+      activeItem: state[ownProps.pageContext].filter(item => item._id === ownProps.match.params.id),
+    };
+  }
+  return {};
+};
+export default withContext(connect(mapStateToProps)(DetailsPage));
